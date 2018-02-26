727e893ebe3997644f10b3866c992c3e670960be
viktharien <viktharien@zoho.com>
2018-02-25 18:50:35 -0600

    install react-rails

--- alpha/app/assets/javascripts/components/appointments.jsx
+++ alpha/app/assets/javascripts/components/appointments.jsx
@@ -1,7 +1,5 @@
-var Appointments = React.createClass({
-    render: function() {
-        return (
-            <h1>Hello World</h1>
-        )
+class Appointments extends React.Component() {
+    render() {
+        return <h1>Test</h1>
     }
-});
+}
