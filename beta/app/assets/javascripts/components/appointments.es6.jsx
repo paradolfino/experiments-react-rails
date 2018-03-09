fa5b81ba3ac9aef743da25982e7dfef9cf89948e
viktharien <viktharien@zoho.com>
2018-03-08 20:45:58 -0600

    begin work on appointments component

--- beta/app/assets/javascripts/components/appointments.es6.jsx
+++ beta/app/assets/javascripts/components/appointments.es6.jsx
@@ -1,7 +1,9 @@
 class Appointments extends React.Component {
     render() {
         return (
+
             <div>
+                <AppointmentForm />
                 {this.props.appointments.map((appointment)=>{
                     return (
                         <Appointment appointment={appointment}/>
