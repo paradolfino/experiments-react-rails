a97ab84488d143a88d7c61380a444d9bd89ae2a2
viktharien <viktharien@zoho.com>
2018-03-08 20:36:08 -0600

    begin work on appointments component

--- beta/app/assets/javascripts/components/appointments.es6.jsx
+++ beta/app/assets/javascripts/components/appointments.es6.jsx
@@ -1,5 +1,9 @@
 class Appointments extends React.Component {
     render() {
-        return <h1>Beta</h1>;
+        return (
+            <div>
+                
+            </div>
+        );
     }
 }
