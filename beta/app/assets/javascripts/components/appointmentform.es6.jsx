e4083a039bf63838cacf431843bc51e4492395e0
viktharien <viktharien@zoho.com>
2018-03-08 20:48:29 -0600

    begin work on appointments component

--- beta/app/assets/javascripts/components/appointmentform.es6.jsx
+++ beta/app/assets/javascripts/components/appointmentform.es6.jsx
@@ -1,11 +1,14 @@
 class AppointmentForm extends React.Component {
     render() {
         return (
-            <form>
-                <input name='title' placeholder='Appointment Title'/>
-                <input name='appt_time' placeholder='Appointment Time'/>
-                <input type="submit" value="Make Appointment"/>
-            </form>
+            <div>
+                <h2>Make an Appointment</h2>
+                <form>
+                    <input name='title' placeholder='Appointment Title'/>
+                    <input name='appt_time' placeholder='Appointment Time'/>
+                    <input type="submit" value="Make Appointment"/>
+                </form>
+            </div>
         );
     }
 }
